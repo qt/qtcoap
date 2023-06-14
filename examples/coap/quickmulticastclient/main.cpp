90d5970c567d966333eaa654fe65cfe58e677ff3
Ivan Solovev <ivan.solovev@qt.io>
2023-06-14 13:39:25 +0200

    CoAP Multicast Discovery example: use modern C++ type registration in QML
    ... and also update build files to use QML modules
    
    Task-number: QTBUG-113858
    Change-Id: I6dbbe05abc0ec345f923f5b2ff5c95cdacda234c
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    (cherry picked from commit cb3d9c032cd5319120df78ce5b9d11b4a3cd9855)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/coap/quickmulticastclient/main.cpp
+++ examples/coap/quickmulticastclient/main.cpp
@@ -1,8 +1,6 @@
 // Copyright (C) 2019 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
-#include "qmlcoapmulticastclient.h"
-
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
@@ -13,21 +11,10 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-
-    qmlRegisterType<QmlCoapMulticastClient>("CoapMulticastClient", 1, 0, "CoapMulticastClient");
-
-    // Register the QtCoap namespace
-    qmlRegisterUncreatableMetaObject(QtCoap::staticMetaObject, "qtcoap.example.namespace", 1, 0,
-                                     "QtCoap", "Access to enums is read-only");
-
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        // Exit with error, if the object for main.qml could not be loaded.
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
+                     &app, []() { QCoreApplication::exit(1); },
+                     Qt::QueuedConnection);
+    engine.loadFromModule("CoapClientModule", "Main");
 
     return app.exec();
 }
