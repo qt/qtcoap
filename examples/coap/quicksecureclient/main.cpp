5b43cd8ed01518426d09ec0716c16be5af6b98c9
Ivan Solovev <ivan.solovev@qt.io>
2023-06-16 14:03:44 +0200

    Secure CoAP Client: use modern C++ type registration in QML
    ... and also update build files to use QML modules
    
    Task-number: QTBUG-113858
    Change-Id: I522c0c8785e85e99f94feedaa9954a232b677da6
    Reviewed-by: Sami Shalayel <sami.shalayel@qt.io>
    (cherry picked from commit 06a0f295bee49d1c8362ccbd8db9d755694a24ff)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/coap/quicksecureclient/main.cpp
+++ examples/coap/quicksecureclient/main.cpp
@@ -1,8 +1,6 @@
 // Copyright (C) 2019 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
-#include "qmlcoapsecureclient.h"
-
 #include <QGuiApplication>
 #include <QNetworkInterface>
 #include <QQmlApplicationEngine>
@@ -27,24 +25,12 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
-
-    qmlRegisterType<QmlCoapSecureClient>("CoapSecureClient", 1, 0, "CoapSecureClient");
-
-    // Register the QtCoap namespace
-    qmlRegisterUncreatableMetaObject(QtCoap::staticMetaObject, "qtcoap.example.namespace", 1, 0,
-                                     "QtCoap", "Access to enums is read-only");
-
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        // Exit with error, if the object for main.qml could not be loaded.
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-
     engine.rootContext()->setContextProperty("hostsModel", availableHosts());
 
-    engine.load(url);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
+                     &app, []() { QCoreApplication::exit(1); },
+                     Qt::QueuedConnection);
+    engine.loadFromModule("CoapSecureClientModule", "Main");
 
     return app.exec();
 }
--- examples/coap/quicksecureclient/qmlcoapsecureclient.h
+++ examples/coap/quicksecureclient/qmlcoapsecureclient.h
@@ -4,12 +4,19 @@
 #ifndef QMLCOAPSECURECLIENT_H
 #define QMLCOAPSECURECLIENT_H
 
-#include <QCoapClient>
+#include <QtCoap/qcoapnamespace.h>
 #include <QCoapSecurityConfiguration>
+
+#include <QtQml/qqmlregistration.h>
+
+QT_BEGIN_NAMESPACE
+class QCoapClient;
+QT_END_NAMESPACE
 
 class QmlCoapSecureClient : public QObject
 {
     Q_OBJECT
+    QML_NAMED_ELEMENT(CoapSecureClient)
 
 public:
     QmlCoapSecureClient(QObject *parent = nullptr);
@@ -31,5 +38,12 @@ private:
     QCoapSecurityConfiguration m_configuration;
     QtCoap::SecurityMode m_securityMode;
 };
+
+namespace QCoapForeignNamespace
+{
+    Q_NAMESPACE
+    QML_FOREIGN_NAMESPACE(QtCoap)
+    QML_NAMED_ELEMENT(QtCoap)
+}
 
 #endif // QMLCOAPSECURECLIENT_H
